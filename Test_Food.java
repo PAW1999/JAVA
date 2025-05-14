d6da2f39261be3f53e0c21b1f7e647a7acde5a78
PAWAN <pawankumar43531@gmail.com>
2025-05-14 18:46:07 +0530

    Add files via upload

---
+++ Test_Food.java
@@ -0,0 +1,14 @@
+class Test_Food{
+	public static void main(String[] args) 
+	{
+	/*	 int item_name;
+	 int quality;
+	 int quantity;
+	 double amount;*/
+		FoodDelevery fd=new FoodDelevery( );
+		
+		fd.display();
+		
+		System.out.println("Hello World!");
+	}
+}
